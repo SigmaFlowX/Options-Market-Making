0f1a44e71c422a1ca280c0cfb705415562922da3
dmitrii <bakaevdima4@gmail.com>
2026-02-12 12:50:51 +0300

    Added evaluation date to solve equation on historical data

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -1,15 +1,15 @@
 import QuantLib as ql
 
-def solve_black_scholes(spot_price, strike_price, risk_free_rate, volatility, expiry_date):
+def solve_black_scholes(spot_price, strike_price, risk_free_rate, volatility, expiry_date, eval_date):
     calendar = ql.TARGET()
     day_count = ql.Actual365Fixed()
-    today = ql.Date().todaysDate()
+    ql.Settings.instance().evaluationDate = eval_date
 
     spot_handle = ql.QuoteHandle(ql.SimpleQuote(spot_price))
-    flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(ql.Date().todaysDate(), risk_free_rate, day_count))
+    flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(eval_date, risk_free_rate, day_count))
 
-    flat_vol_ts = ql.BlackVolTermStructureHandle(ql.BlackConstantVol(ql.Date().todaysDate(), calendar, volatility, day_count))
-    dividend_ts = ql.YieldTermStructureHandle(ql.FlatForward(today, 0.0, day_count)
+    flat_vol_ts = ql.BlackVolTermStructureHandle(ql.BlackConstantVol(eval_date, calendar, volatility, day_count))
+    dividend_ts = ql.YieldTermStructureHandle(ql.FlatForward(eval_date, 0.0, day_count)
     )
     process = ql.BlackScholesMertonProcess(
         spot_handle,
@@ -19,7 +19,7 @@ def solve_black_scholes(spot_price, strike_price, risk_free_rate, volatility, ex
     )
 
     payoff = ql.PlainVanillaPayoff(ql.Option.Call, strike_price)
-    exercise = ql.AmericanExercise(ql.Date().todaysDate(), expiry_date)
+    exercise = ql.AmericanExercise(eval_date, expiry_date)
 
     american_option = ql.VanillaOption(payoff, exercise)
 
@@ -38,5 +38,6 @@ if __name__ == "__main__":
     expiry = ql.Date(20, 2, 2026)
     rf = 0.15
     volatility = 0.2
+    eval_date = ql.Date(12, 2, 2026)
 
-    print(solve_black_scholes(price, strike, rf, volatility, expiry))
+    print(solve_black_scholes(price, strike, rf, volatility, expiry, eval_date))
