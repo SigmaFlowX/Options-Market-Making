3dd3f7fdf8ffb1393613edee74e37ee2ba455b46
dmitrii <bakaevdima4@gmail.com>
2026-02-18 16:06:48 +0300

    created MarketData class and added async authorize function

--- src/mm_engine.py
+++ src/mm_engine.py
@@ -1,1 +1,41 @@
 import asyncio
+import os
+import aiohttp
+
+class MarketData:
+    def __init__(self, token):
+        self.refresh_token = token
+        self.access_token = None
+
+        self.q_orderbooks = asyncio.Queue()
+
+    async def authorize(self):
+        url = "https://be.broker.ru/trade-api-keycloak/realms/tradeapi/protocol/openid-connect/token"\
+
+        payload = {
+            "client_id": "trade-api-write",
+            "refresh_token": self.refresh_token,
+            "grant_type": "refresh_token"
+        }
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json"
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, data=payload, timeout=10) as resp:
+                data = await resp.json()
+                self.refresh_token = data['access_token']
+                print("O")
+
+
+
+
+
+
+#env variable
+bks_token = os.getenv("BKS_TOKEN")
+market_data = MarketData(bks_token)
+
+asyncio.run(market_data.authorize())
