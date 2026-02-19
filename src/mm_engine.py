edc6d1851400a7f0b0cf9a4540cadb063c14814b
dmitrii <bakaevdima4@gmail.com>
2026-02-19 16:03:35 +0300

    now only opening session once when starting BrokerClient and not every time

--- src/mm_engine.py
+++ src/mm_engine.py
@@ -6,10 +6,16 @@ import json
 class BrokerClient:
     def __init__(self, token):
         self.refresh_token = token
+        self.session = None
         self.access_token = None
 
         self.q_inventory = asyncio.Queue()
         self.q_orderbooks = asyncio.Queue()
+
+    async def start(self):
+        self.session = aiohttp.ClientSession()
+        await self.authorize()
+
 
     async def authorize(self):
         url = "https://be.broker.ru/trade-api-keycloak/realms/tradeapi/protocol/openid-connect/token"\
@@ -27,18 +33,17 @@ class BrokerClient:
 
         for attempt in range(4):
             try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.post(url, headers=headers, data=payload, timeout=10) as resp:
-                        if resp.status!= 200:
-                            text = await resp.text()
-                            print(f"Invalid response while authorizing \n {resp.status} \n {text}")
-                            await asyncio.sleep(3 + 2*attempt)
-                            attempt += 1
-                            continue
-                        data = await resp.json()
-                        self.access_token = data['access_token']
-                        print("Authorized")
-                        return
+                async with self.session.post(url, headers=headers, data=payload, timeout=10) as resp:
+                    if resp.status!= 200:
+                        text = await resp.text()
+                        print(f"Invalid response while authorizing \n {resp.status} \n {text}")
+                        await asyncio.sleep(3 + 2*attempt)
+                        attempt += 1
+                        continue
+                    data = await resp.json()
+                    self.access_token = data['access_token']
+                    print("Authorized")
+                    return
             except (aiohttp.ClientError, asyncio.TimeoutError) as e:
                 print(f"Failed attempt {attempt+1} while authorizing: \n {e}")
                 await asyncio.sleep(3 + 2*attempt)
@@ -52,35 +57,35 @@ class BrokerClient:
         attempt = 0
         while True:
             try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.ws_connect(url, headers=headers) as ws:
-                        subscribe_message = {
-                            "subscribeType": 0,
-                            "dataType": 0,
-                            "depth": depth,
-                            "instruments": [
-                                {
-                                    "classCode": class_code,
-                                    "ticker": ticker
-                                }
-                            ]
-                        }
-                        await ws.send_json(subscribe_message)
-                        print(f"connected ws for {ticker}")
-                        async for msg in ws:
-                            if msg.type == aiohttp.WSMsgType.TEXT:
-                                try:
-                                    data = json.loads(msg.data)
-                                except Exception as e:
-                                    print("Invalid json")
-                                    continue
-                                await self.q_orderbooks.put(data)
-                            elif msg.type == aiohttp.WSMsgType.ERROR:
-                                print(f"Websocket message error: \n {ws.exception()}")
-                                break
-                            elif msg.type in (aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.CLOSING):
-                                print("Websocket closed by server")
-                                break
+                async with self.session.ws_connect(url, headers=headers) as ws:
+                    subscribe_message = {
+                        "subscribeType": 0,
+                        "dataType": 0,
+                        "depth": depth,
+                        "instruments": [
+                            {
+                                "classCode": class_code,
+                                "ticker": ticker
+                            }
+                        ]
+                    }
+                    await ws.send_json(subscribe_message)
+                    print(f"connected ws for {ticker}")
+                    async for msg in ws:
+                        if msg.type == aiohttp.WSMsgType.TEXT:
+                            try:
+                                data = json.loads(msg.data)
+                            except Exception as e:
+                                print("Invalid json")
+                                continue
+                            print(data)
+                            await self.q_orderbooks.put(data)
+                        elif msg.type == aiohttp.WSMsgType.ERROR:
+                            print(f"Websocket message error: \n {ws.exception()}")
+                            break
+                        elif msg.type in (aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.CLOSING):
+                            print("Websocket closed by server")
+                            break
 
             except (aiohttp.ClientError, asyncio.TimeoutError) as e:
                 print(f"Failed attempt {attempt + 1} while opening order book websocket: \n {e}")
@@ -98,25 +103,24 @@ class BrokerClient:
         attempt = 0
         while True:
             try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url, headers=headers, data=payload) as resp:
-                        if resp.status!= 200:
-                            text = await resp.text()
-                            print(f"Invalid response while updating inventory \n {resp.status} \n {text}")
-                            await asyncio.sleep(3 + 2*attempt)
-                            attempt += 1
+                async with self.session.get(url, headers=headers, data=payload) as resp:
+                    if resp.status!= 200:
+                        text = await resp.text()
+                        print(f"Invalid response while updating inventory \n {resp.status} \n {text}")
+                        await asyncio.sleep(3 + 2*attempt)
+                        attempt += 1
+                        continue
+                    data = await resp.json()
+                    inventory = {}
+                    for position in data:
+                        ticker = position['ticker']
+                        if ticker in inventory:
                             continue
-                        data = await resp.json()
-                        inventory = {}
-                        for position in data:
-                            ticker = position['ticker']
-                            if ticker in inventory:
-                                continue
-                            size = position['quantity']
-                            inventory[ticker] = size
-                        await self.q_inventory.put(inventory)
-                        print(inventory)
-                        return inventory
+                        size = position['quantity']
+                        inventory[ticker] = size
+                    await self.q_inventory.put(inventory)
+                    print(inventory)
+                    return inventory
 
             except (aiohttp.ClientError, asyncio.TimeoutError) as e:
                 print(f"Failed attempt {attempt + 1} while opening updating inventory: \n {e}")
@@ -129,9 +133,13 @@ async def main():
     token = os.getenv("BKS_TOKEN")
     client = BrokerClient(token)
 
-    await client.authorize()
-    ws_task = asyncio.create_task(client.get_inventory())
-    await ws_task
+    await client.start()
+    tasks = [
+        client.get_inventory(),
+        client.start_order_book_ws(ticker="SR300CB6D", depth=1, class_code="OPTSPOT" )
+
+    ]
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
