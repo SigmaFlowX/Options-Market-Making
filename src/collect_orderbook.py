bec5fa4738b5cde3d94f6231896bc9447a568dd1
dmitrii <bakaevdima4@gmail.com>
2026-03-11 15:54:04 +0300

    added exceptions handling for robustness

--- src/collect_orderbook.py
+++ src/collect_orderbook.py
@@ -3,6 +3,7 @@ import os
 import json
 import asyncio
 
+
 TICKERS = [
     ("SR320CC6", "OPTSPOT")
 ]
@@ -15,21 +16,31 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
 
 async def save_orderbook_data(client):
     while True:
-        data = await client.q_orderbooks.get()
-        if data['responseType'] == "OrderBook":
-            ticker = data['ticker']
-            output_file = os.path.join(DATA_DIR, f"{ticker}.jsonl")
-            with open(output_file, "a") as f:
-                json.dump(data, f)
-                f.write("\n")
+        try:
+            data = await client.q_orderbooks.get()
+            if data['responseType'] == "OrderBook":
+                ticker = data['ticker']
+                output_file = os.path.join(DATA_DIR, f"{ticker}.jsonl")
+                with open(output_file, "a") as f:
+                    json.dump(data, f)
+                    f.write("\n")
 
-            print(f"Updated {ticker}")
-
+                print(f"Updated {ticker}")
+        except Exception as e:
+            print(f"Error while saving: {e}")
+            await asyncio.sleep(10)
 
 async def main():
     token = os.getenv("BKS_TOKEN")
     client = BrokerClient(token)
-    await client.start()
+
+    while True:
+        try:
+            await client.start()
+            break
+        except Exception as e:
+            print(f"Exception while starting a client {e}")
+            await asyncio.sleep(10)
 
     save_task = asyncio.create_task(save_orderbook_data(client))
     ws_tasks = []
