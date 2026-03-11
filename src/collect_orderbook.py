1bd50bb2da46bc5f1133bba7c93a8910cd9d6ca4
dmitrii <bakaevdima4@gmail.com>
2026-03-11 15:49:24 +0300

    added multiple pairs tracking

--- src/collect_orderbook.py
+++ src/collect_orderbook.py
@@ -3,38 +3,43 @@ import os
 import json
 import asyncio
 
-TICKER = "SR320CC6"
+TICKERS = [
+    ("SR320CC6", "OPTSPOT")
+]
 DEPTH = 5
-CLASS_CODE = "OPTSPOT"
+
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
-OUTPUT_FILE = os.path.join(DATA_DIR, f"{TICKER}.jsonl")
+
 
 
 async def save_orderbook_data(client):
     while True:
         data = await client.q_orderbooks.get()
         if data['responseType'] == "OrderBook":
-            with open(OUTPUT_FILE, "a") as f:
+            ticker = data['ticker']
+            output_file = os.path.join(DATA_DIR, f"{ticker}.jsonl")
+            with open(output_file, "a") as f:
                 json.dump(data, f)
                 f.write("\n")
 
+            print(f"Updated {ticker}")
+
+
 async def main():
-    while True:
-        try:
-            token = os.getenv("BKS_TOKEN")
-            client = BrokerClient(token)
+    token = os.getenv("BKS_TOKEN")
+    client = BrokerClient(token)
+    await client.start()
 
-            await client.start()
-            task_save = asyncio.create_task(save_orderbook_data(client))
-            task_ws = asyncio.create_task(client.start_order_book_ws(TICKER, DEPTH, CLASS_CODE))
-            await asyncio.gather(task_save, task_ws)
+    save_task = asyncio.create_task(save_orderbook_data(client))
+    ws_tasks = []
+    for ticker, class_code in TICKERS:
+        ws_tasks.append(asyncio.create_task(client.start_order_book_ws(ticker, DEPTH, class_code)))
 
-
-        except Exception as e:
-            print(f"Exception {e} occurred")
-            await asyncio.sleep(10)
-
+    try:
+        await asyncio.gather(save_task, *ws_tasks)
+    finally:
+        await client.close()
 
 
 if __name__ == "__main__":
