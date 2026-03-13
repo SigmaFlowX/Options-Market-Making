553e31a19611f6442050a245b80a41b315c30172
dmitrii <bakaevdima4@gmail.com>
2026-03-13 17:23:20 +0300

    added client refreshed since token only lives for 24 hours

--- src/collect_live_orderbook_data.py
+++ src/collect_live_orderbook_data.py
@@ -19,6 +19,21 @@ DEPTH = 5
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
 
+async def refresh_client(client):
+    await asyncio.sleep(60)
+    while True:
+        while True:
+            try:
+                await client.close()
+                await client.start()
+                print("client refreshed")
+                break
+            except Exception as e:
+                print(f"Exception {e}")
+                await asyncio.sleep(10)
+
+
+
 async def connect_db():
     conn = await asyncpg.connect(os.getenv("DATABASE_URL"))
     return conn
@@ -59,7 +74,7 @@ async def save_orderbook_data(client, conn):
                     data["askVolume"]
                 )
 
-                print(f"Updated {data["ticker"]}")
+                print(f"Updated {data['ticker']}")
         except Exception as e:
             print(f"Error while saving: {e}")
             await asyncio.sleep(10)
@@ -79,12 +94,13 @@ async def main():
             await asyncio.sleep(10)
 
     save_task = asyncio.create_task(save_orderbook_data(client, conn))
+    refresh_task = asyncio.create_task(refresh_client(client))
     ws_tasks = []
     for ticker, class_code in TICKERS:
         ws_tasks.append(asyncio.create_task(client.start_order_book_ws(ticker, DEPTH, class_code)))
 
     try:
-        await asyncio.gather(save_task, *ws_tasks)
+        await asyncio.gather(save_task, *ws_tasks, refresh_task)
     finally:
         await client.close()
 
