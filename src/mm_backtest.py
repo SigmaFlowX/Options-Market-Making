6e5a4a673740a2311758b268bbac56e2549630d7
Dmitrii <bakaevdima4@gmail.com>
2026-03-21 14:57:05 +0300

    started working on the backtest script, added simple order generator from mm_engine

---
+++ src/mm_backtest.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+def generate_orders_simple(mid, best_bid, best_ask, inventory, inventory_k, order_size, inventory_limit):
+
+    half_spread = abs((best_ask - best_bid)) / 2
+
+    inventory_shift = inventory_k * inventory
+    center = mid - inventory_shift
+
+    bid = center - half_spread + 0.01
+    ask = center + half_spread - 0.01
+
+    bid = min(bid, best_bid)
+    ask = max(ask, best_ask)
+
+    bid_size = order_size
+    ask_size = order_size
+
+    if inventory > 0:
+        bid_size *= max(0.1, 1 - abs(inventory) / inventory_limit)
+
+    if inventory < 0:
+        ask_size *= max(0.1, 1 - abs(inventory) / inventory_limit)
+
+    bid_size = max(1, bid_size)
+    ask_size = max(1, ask_size)
+
+    if ask_size > inventory:
+        ask_size = inventory
+
+    if inventory == 0.0:
+        ask_size = 0
+    if inventory >= inventory_limit:
+        bid_size = 0
+    elif inventory <= inventory_limit:
+        ask_size = 0
+
+    ask_order = {
+        "price": round(ask, 2),
+        "quantity": round(ask_size)
+    }
+
+    bid_order = {
+        "price": round(bid, 2),
+        "quantity": round(bid_size)
+    }
+
+    orders = {}
+    if bid_size > 0 and inventory < inventory_limit:
+        orders['ask'] = ask_order
+    if ask_size > 0 and inventory > 0:
+        orders['bid'] = bid_order
+    return orders if orders else None
+
+def run_backtest(df):
+    pass
