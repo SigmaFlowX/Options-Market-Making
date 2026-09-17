84d192ad98cd648181cfc9c4ed0bd46724b164bb
Dmitrii <bakaevdima4@gmail.com>
2026-09-17 18:17:43 +0300

    updated ticker list

--- src/collect_live_data.py
+++ src/collect_live_data.py
@@ -15,7 +15,11 @@ postfix_list = [
     "CI6", #16th september
     "CU6",
     "CU6D", #23rd september
-    "CI6D"
+    "CI6D",
+    "CJ6A", #30rd september
+    "CV6A",
+    "CJ6", #21rd october
+    "CV6"
 ]
 
 ticker = "SR"
